a7a839835b072f4069b8b48b73d3d8b1e3f02a60
aaroncaffrey <aaroncaffrey@gmail.com>
2020-02-21 00:42:13 +0000

    updates

--- svm_fs/pub.sh
+++ svm_fs/pub.sh
@@ -4,5 +4,14 @@ rm -rf ~/svm_fs/svm_fs/pbs_ldr_sub
 rm -rf ~/svm_fs/svm_fs/results
 rm -rf ~/svm_fs/svm_fs/obj
 rm -rf ~/svm_fs/svm_fs/bin
+
+rm -rf /mmfs1/data/scratch/k1040015/svm_fs/pbs_ctl_sub
+rm -rf /mmfs1/data/scratch/k1040015/svm_fs/pbs_wkr_sub
+rm -rf /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub
+rm -rf /mmfs1/data/scratch/k1040015/svm_fs/results
+rm -rf /mmfs1/data/scratch/k1040015/svm_fs/obj
+rm -rf /mmfs1/data/scratch/k1040015/svm_fs/bin
+
+
 ~/.dotnet/dotnet publish --self-contained -r linux-x64 -c Release
 cd ~/svm_fs/svm_fs/bin/Release/netcoreapp3.1/linux-x64/publish/
