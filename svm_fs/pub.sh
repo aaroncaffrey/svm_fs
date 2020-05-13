9cb0dbfef3362476e80acef4930174fccfefbf56
aaroncaffrey <aaroncaffrey@gmail.com>
2020-05-13 04:19:57 +0100

    update

--- svm_fs/pub.sh
+++ svm_fs/pub.sh
@@ -20,11 +20,15 @@ mkdir /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub
 mkdir /mmfs1/data/scratch/k1040015/svm_fs/pbs_ctl_sub
 mkdir /mmfs1/data/scratch/k1040015/svm_fs/pbs_wkr_sub
 
-~/.dotnet/dotnet publish --self-contained -r linux-x64 -c Release
+# ~/.dotnet/dotnet publish --self-contained -r linux-x64 -c Release
+msub -K compile.pbs
+
 cd ~/svm_fs/svm_fs/bin/Release/netcoreapp3.1/linux-x64/publish/
 
 echo Running
+
 # nohup ~/svm_fs/svm_fs/bin/Release/netcoreapp3.1/linux-x64/publish/svm_fs -cm ldr 1> /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub/svm_ldr.stdout 2> /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub/svm_ldr.stderr &
-~/svm_fs/svm_fs/bin/Release/netcoreapp3.1/linux-x64/publish/svm_fs -cm ldr 1> /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub/svm_ldr.stdout 2> /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub/svm_ldr.stderr
+echo ~/svm_fs/svm_fs/bin/Release/netcoreapp3.1/linux-x64/publish/svm_fs -cm ldr -en vcpu_1_1056 -pc 1 -pt 1056 1> /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub/svm_ldr.stdout 2> /mmfs1/data/scratch/k1040015/svm_fs/pbs_ldr_sub/svm_ldr.stderr
+
 echo Finished
 
